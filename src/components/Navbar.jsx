4032222a82d374c94f0f10fdd6ea6b51616753e5
dilbarqoldosheva <bintushokir07@gmail.com>
2025-02-08 11:20:49 +0500

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+//react imports
+import { useEffect, useState } from "react"
 
+//rrd imports
+import { Link, useParams } from "react-router-dom"
+const toggleMode = () =>{
+    return localStorage.getItem("darkMode") || "light"
+}
 function Navbar() {
+ const {title} = useParams()
+ const [theme, setTheme] = useState('light')
+ //theme toggle function
+ const handleThemeToggle =()=>{
+    const newTheme = theme == "dark-mode" ? "light" : "dark-mode"
+    setTheme(newTheme)
+ }
+ 
+ useEffect(()=>{
+    document.body.classList = ''
+    document.body.classList.add(theme)
+    localStorage.setItem("darkMode", theme)
+ },[theme])
   return (
-    <div>Navbar</div>
+    <header className="header">
+     <div className='header-container container'>
+        <div>
+            {title && (
+            <Link className="header-logo" to="/">
+                <figure>
+                    <img src={`../assets/icon-${title.toLocaleLowerCase()}.svg`} alt={`${title} icon`} />
+                </figure>
+                <span>{title}</span>
+            </Link>
+        ) }
+        </div>
+        <div className='dark-btn' onClick={handleThemeToggle}>
+            <input type="checkbox" checked={theme == "dark-mode"} readOnly />
+            <span>
+                <span></span>
+                <span></span>
+            </span>
+        </div>
+    </div>
+    </header>
   )
 }
 
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+//rrd  import 
+import { useParams } from "react-router-dom"
 
+//custom hooks
+import {useFetch} from "../hooks/useFetch"
+
+//react hooks
+import { useEffect } from "react"
+
+//components
+import Test from "../components/Test"
 function Quiz() {
-  return (
-    <div>Quiz</div>
-  )
+  const {title} = useParams()
+  const {data:quizzes, isPending, error } = useFetch(`http://localhost:3000/quizzes?title=${title}`)
+  console.log(quizzes, "wf");
+  
+  useEffect(()=>{
+   document.title = "Quiz" + " " + title
+  },[title])
+  return <section className="quiz-container container"> 
+  {isPending && <h3>Loading...</h3>}
+  {error && <h3>{error}</h3>}
+  {quizzes && <h3><Test questions={quizzes[0]}/></h3>}
+  </section>
 }
 
 export default Quiz
